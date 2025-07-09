b2c4128ef5282bedc298b3bf5d9f4b241e9b932c
RicheTheDev <uassogba06@gmail.com>
2025-07-09 11:09:00 +0200

    all commit

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///techfuturists.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
  
     MAIL_SERVER = 'smtp.gmail.com'
